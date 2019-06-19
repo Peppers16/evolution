623e7c24e68892293604b52676d04df66beb4f2a
Josh <joshdh81691@gmail.com>
2019-06-19 11:20:53 +0100

    Board class with random flora spawning and remove_flora method. Set to track list of available squares.

--- evolution.py
+++ evolution.py
@@ -1,7 +1,37 @@
-def create_board(size=100):
-    """This board will represent the ecosystem as a square matrix"""
-    board = [[None] * size for i in range(size)]
-    return board
+import random
+
+
+class Board:
+    def __init__(self, size=100, start_flora=0):
+        self.board = [[None] * size for i in range(size)]
+        self.size = size
+        # counts
+        self.flora_count = 0
+        self.creature_count = 0
+        # sets and lists for tracking locations and creatures
+        self.free_squares = set()
+        for i in range(size):
+            for j in range(size):
+                self.free_squares.add((i, j))
+        self.creature_list = []
+        self.spawn_flora(start_flora)
+
+    def spawn_flora(self, n_flora):
+        """n_flora will be added to the board randomly, subject to free squares being available"""
+        for i in range(n_flora):
+            if len(self.free_squares):  # if set is non-empty
+                x, y = random.choice(tuple(self.free_squares))
+                self.board[x][y] = 'F'
+                self.flora_count += 1
+                self.free_squares.remove((x, y))
+
+    def remove_flora(self, x, y):
+        """remove a single flora from the board, and free up the space"""
+        if self.board[x][y] != 'F':
+            raise Exception("Attempted to remove flora from location where no flora was present")
+        self.board[x][y] = None
+        self.flora_count -= 1
+        self.free_squares.add((x, y))
 
 
 class Creature:
--- test_evolution.py
+++ test_evolution.py
@@ -2,18 +2,38 @@ from evolution import *
 
 
 def test_board():
-    t_board = create_board(3)
-    assert t_board == \
+    t_board = Board(3, start_flora=0)
+    assert t_board.board == \
            [[None, None, None]
             , [None, None, None]
             , [None, None, None]]
+    assert t_board.flora_count == 0
+    assert len(t_board.free_squares) == 9
+    assert t_board.size == 3
 
-    t_board[1][1] = 'X'
 
-    assert t_board == \
-           [[None, None, None]
-            , [None, 'X', None]
-            , [None, None, None]]
+def test_spawn_flora():
+    t_board = Board(3, start_flora=0)
+    assert t_board.flora_count == 0
+    assert len(t_board.free_squares) == 9
+
+    t_board.spawn_flora(3)
+
+    assert t_board.flora_count == 3
+    assert len(t_board.free_squares) == 6
+
+    print(t_board.board)
+
+
+def test_remove_flora():
+    test_board = Board(6, start_flora=36)
+    assert len(test_board.free_squares) == 0
+    assert test_board.flora_count == 36
+    assert test_board.board[1][2] == 'F'
+    test_board.remove_flora(1, 2)
+    assert len(test_board.free_squares) == 1
+    assert test_board.flora_count == 35
+    assert test_board.board[1][2] is None
 
 
 def test_creature():
